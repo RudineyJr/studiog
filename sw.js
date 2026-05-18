2fe4ceb17f7c84d6364678a17844b8fdd097025b
RudineyJr <54280360+RudineyJr@users.noreply.github.com>
2026-05-18 07:40:07 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,10 +4,19 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE).then(c =>
       c.addAll([
-        './'
+        './',
+        './index.html',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png'
       ])
-    )
+    ).then(() => self.skipWaiting()) // <-- Força a instalação imediata
   );
+});
+
+self.addEventListener('activate', e => {
+  // Força o Service Worker atualizado a tomar o controle das abas abertas imediatamente
+  e.waitUntil(self.clients.claim()); 
 });
 
 self.addEventListener('fetch', e => {
