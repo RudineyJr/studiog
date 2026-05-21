24b6a348cfb64e2f3d37f6fed054ad4e11b9d785
RudineyJr <54280360+RudineyJr@users.noreply.github.com>
2026-05-21 17:04:30 -0400

    Update app.js

--- app.js
+++ app.js
@@ -8,3 +8,15 @@
   navigator.serviceWorker.register('./sw.js');
 }
 </script>
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("./sw.js");
+  });
+}
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("./sw.js");
+  });
+}
